b18c125e3e664dc16f425bfe475c26e5319e2973
WyvernDrexx <lilaprashadsharma@gmail.com>
2022-02-07 20:28:31 +0530

    comple.js remove unwanted logs

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -9,19 +9,16 @@ const buildPath = path.resolve(__dirname, 'build')
 /** Read the contract file */
 const source = fs.readFileSync(contractPath, 'utf-8')
 /** Compile the Solidity code */
-console.log('[COMPILE]:', contractPath)
 const compiledSource = solc.compile(source, 1).contracts
 /** Remove the build directory */
-console.log('[REMOVE]:', buildPath)
 fs.removeSync(buildPath)
 /** Re-Create the build directory before writing */
-console.log('[CREATE]:', buildPath)
 fs.ensureDirSync(buildPath)
 /** Loop over the compiled source to create separate build files for each contract */
 for (const contract in compiledSource) {
   /** Create the file path inside build */
-  const filePath = path.resolve(buildPath, contract.split(':')[1] + '.json')
+  const filePath = path.resolve(buildPath, contract.replace(':', '') + '.json')
   /** Save the output into file */
-  console.log('[BUILD Contract]:', contract.split(':')[1])
+  console.log(`[BUILD Contract]: [${contract.replace(':', '')}] into file: [${filePath}]`)
   fs.outputJSONSync(filePath, compiledSource[contract])
 }
